6c9ae57a698ce0676abbd3a6c5ae0b5e76e31156
pureiboi <17396188+pureiboi@users.noreply.github.com>
2026-02-21 01:44:51 +0800

    count group and unloaded tab (#4)
    feat: option to count unloaded tab and count group

--- src/common.js
+++ src/common.js
@@ -5,10 +5,18 @@ export const COUNT_TAB_ALL_WINDOWS = 1
 export const COUNT_TAB_CURRENT_WINDOW_OVER_ALL_WINDOWS = 2
 export const COUNT_WINDOW = 4
 export const COUNT_NONE = 3
+export const COUNT_ALL_GROUPS = 5
+export const COUNT_CURRENT_WINDOW_GROUPS = 6
 
 export const STAT_WINDOW_COUNT = 'all_windows'
 export const STAT_ALL_TAB_COUNT = 'all_tabs'
 export const STAT_CURRENT_WINDOW_TABS_COUNT = 'current_window_tabs'
+export const STAT_CURRENT_WINDOW_GROUPS_COUNT = 'current_window_groups'
+export const STAT_ALL_GROUP_COUNT = 'all_groups'
+export const STAT_CURRENT_TAB_GROUP_COUNT = 'current_window_tab_group'
+export const STAT_ALL_TAB_GROUP_COUNT = 'all_tab_group'
+export const STAT_CURRENT_UNLOADED_TAB_COUNT = 'current_window_unloaded_tab'
+export const STAT_ALL_UNLOADED_TAB_COUNT = 'all_unloaded_tab'
 
 export function getBaseQuery () {
   return {
@@ -43,6 +51,17 @@ export function getSkipHiddenTabQuery (skipHiddenTabPreference) {
   }
 }
 
+function formatQueryUnloadedTab () {
+  return {
+    allTabsQuery: {
+      discarded: true
+    },
+    currentWindowQuery: {
+      discarded: true
+    }
+  }
+}
+
 export async function queryBadgeData (windowId) {
   const settings = await browser.storage.local.get()
 
@@ -54,11 +73,28 @@ export async function queryBadgeData (windowId) {
   })
   const allTabObj = await browser.tabs.query(queryObject.allTabsQuery)
   const currentWindowTab = await browser.tabs.query(lodash.merge(queryObject.currentWindowQuery, { windowId }))
+  const currentWindowGroupCount = await browser.tabGroups.query({ windowId })
+  const allGroupObj = await browser.tabGroups.query({})
+  const currentWindowTagInGroup = currentWindowTab.filter(tab => tab.groupId !== browser.tabGroups.TAB_GROUP_ID_NONE)
+  const allTagInGroup = allTabObj.filter(tab => tab.groupId !== browser.tabGroups.TAB_GROUP_ID_NONE)
+
+  let allUnloadedTabCount = 0
+  let unloadedCurrentWindowTabCount = 0
+  if (settings.countUnloadedTab) {
+    allUnloadedTabCount = (await browser.tabs.query(formatQueryUnloadedTab().allTabsQuery)).length
+    unloadedCurrentWindowTabCount = (await browser.tabs.query(lodash.merge(formatQueryUnloadedTab().currentWindowQuery, { windowId }))).length
+  }
 
   return {
     [STAT_CURRENT_WINDOW_TABS_COUNT]: currentWindowTab.length.toString(),
     [STAT_WINDOW_COUNT]: allWindowsObj.length.toString(),
-    [STAT_ALL_TAB_COUNT]: allTabObj.length.toString()
+    [STAT_ALL_TAB_COUNT]: allTabObj.length.toString(),
+    [STAT_CURRENT_WINDOW_GROUPS_COUNT]: currentWindowGroupCount.length.toString(),
+    [STAT_ALL_GROUP_COUNT]: allGroupObj.length.toString(),
+    [STAT_CURRENT_TAB_GROUP_COUNT]: currentWindowTagInGroup.length.toString(),
+    [STAT_ALL_TAB_GROUP_COUNT]: allTagInGroup.length.toString(),
+    [STAT_CURRENT_UNLOADED_TAB_COUNT]: unloadedCurrentWindowTabCount.toString(),
+    [STAT_ALL_UNLOADED_TAB_COUNT]: allUnloadedTabCount.toString()
   }
 }
 
@@ -83,16 +119,26 @@ const updateWindowBadge = async function updateWindowBadge (windowId) {
   const statData = await queryBadgeData(windowId)
 
   let text = statData[STAT_CURRENT_WINDOW_TABS_COUNT]
-  if (counterPreference === COUNT_TAB_CURRENT_WINDOW) {
-    // Badge shows current window
-    text = statData[STAT_CURRENT_WINDOW_TABS_COUNT]
-  } else if (counterPreference === COUNT_TAB_ALL_WINDOWS) {
-    // Badge shows total of all windows
-    text = statData[STAT_ALL_TAB_COUNT]
-  } else if (counterPreference === COUNT_TAB_CURRENT_WINDOW_OVER_ALL_WINDOWS) {
-    // Badge shows both (Firefox limits to about 4 characters based on width)
-    text = `${statData[STAT_CURRENT_WINDOW_TABS_COUNT]}/${statData[STAT_ALL_TAB_COUNT]}`
-  } else if (counterPreference === COUNT_WINDOW) text = statData[STAT_WINDOW_COUNT] // Badge shows total of all windows
+  switch (counterPreference) {
+    case COUNT_TAB_CURRENT_WINDOW:
+      text = statData[STAT_CURRENT_WINDOW_TABS_COUNT]
+      break
+    case COUNT_TAB_ALL_WINDOWS:
+      text = statData[STAT_ALL_TAB_COUNT]
+      break
+    case COUNT_TAB_CURRENT_WINDOW_OVER_ALL_WINDOWS:
+      text = `${statData[STAT_CURRENT_WINDOW_TABS_COUNT]}/${statData[STAT_ALL_TAB_COUNT]}`
+      break
+    case COUNT_WINDOW:
+      text = statData[STAT_WINDOW_COUNT] // Badge shows total of all windows
+      break
+    case COUNT_ALL_GROUPS:
+      text = statData[STAT_ALL_GROUP_COUNT]
+      break
+    case COUNT_CURRENT_WINDOW_GROUPS:
+      text = statData[STAT_CURRENT_WINDOW_GROUPS_COUNT]
+      break
+  }
 
   //   Update the badge
   browser.action.setBadgeText({
--- src/popup.js
+++ src/popup.js
@@ -31,6 +31,27 @@ async function start () {
   document.getElementById('currentWindow').textContent = statData[common.STAT_CURRENT_WINDOW_TABS_COUNT]
   document.getElementById('allTabs').textContent = statData[common.STAT_ALL_TAB_COUNT]
   document.getElementById('allWindows').textContent = statData[common.STAT_WINDOW_COUNT]
+  document.getElementById('currentWindowGroups').textContent = statData[common.STAT_CURRENT_WINDOW_GROUPS_COUNT]
+  document.getElementById('allGroups').textContent = statData[common.STAT_ALL_GROUP_COUNT]
+  document.getElementById('allTagGroup').textContent = statData[common.STAT_ALL_TAB_GROUP_COUNT]
+  document.getElementById('currentTagGroup').textContent = statData[common.STAT_CURRENT_TAB_GROUP_COUNT]
+  document.getElementById('allUnloaded').textContent = statData[common.STAT_ALL_UNLOADED_TAB_COUNT]
+  document.getElementById('currentWindowUnloaded').textContent = statData[common.STAT_CURRENT_UNLOADED_TAB_COUNT]
+
+  const settings = await browser.storage.local.get()
+  if (!settings.countUnloadedTab) {
+    const elements = document.getElementsByClassName('unloadedRow')
+    for (const element of elements) {
+      element.classList.add('hide')
+    }
+  }
+
+  if (!settings.countGroup) {
+    const elements = document.getElementsByClassName('groupTabRow')
+    for (const element of elements) {
+      element.classList.add('hide')
+    }
+  }
 }
 
 if (typeof browser === 'undefined') {
